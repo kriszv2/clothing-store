564b4caecc33e54465c2bcef2830dd9ca620cdbf
Krisztian Valyi <krisztian.valyi1@gmail.com>
2024-07-25 11:21:49 +0100

    Added cart context

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,10 +1,21 @@
+import { useContext } from "react";
 import "./cart-icon.styles.scss"
 import StorefrontIcon from '@mui/icons-material/Storefront';
+import { CartContext } from "../../contexts/cart.context";
+
+
+
 
 
 export const CartIcon = () =>{
+
+    const {isCartOpen, setIsCartOpen } = useContext(CartContext)
+
+    const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen)
+
+
     return(
-        <div className="cart-icon-container">
+        <div className="cart-icon-container" onClick={toggleIsCartOpen}>
             <StorefrontIcon className="shopping-icon"/>
             <span className="item-count">0</span>
         </div>
